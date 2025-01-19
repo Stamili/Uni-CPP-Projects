92c8ef85c02353129b76eea14d4c8f0e4d9762dc
Stamili <96841958+Stamili@users.noreply.github.com>
2025-01-19 22:20:45 +0330

    Add helping verb correction

--- A.cpp
+++ A.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main() {
     short int textLength = 501;
-    short int i;
+    short int i, j;
     short int digitSum = 0;
     long long numberSum = 0;
     short int charCount = 0;
@@ -185,9 +185,197 @@ int main() {
         }
     }
 
+    // Helping verb correction.
+    for (i = 0; text[i] != '\0'; i++) {
+    // Correct "he am" to "he is"
+    if ((text[i] == 'h' || text[i] == 'H') && (text[i+1] == 'e' || text[i+1] == 'E') && (text[i+2] == ' ') && (text[i+3] == 'a' || text[i+3] == 'A') && (text[i+4] == 'm' || text[i+4] == 'M') && !((text[i+5] >= 'a' && text[i+5] <= 'z') || (text[i+5] >= 'A' && text[i+5] <= 'Z'))) {
+        text[i+3] = 'i';
+        text[i+4] = 's';
+        i += 5;
+    }
+    
+    // Correct "he are" to "he is"
+    if ((text[i] == 'h' || text[i] == 'H') && (text[i+1] == 'e' || text[i+1] == 'E') && (text[i+2] == ' ') && (text[i+3] == 'a' || text[i+3] == 'A') && (text[i+4] == 'r' || text[i+4] == 'R') && (text[i+5] == 'e' || text[i+5] == 'E') && !((text[i+6] >= 'a' && text[i+6] <= 'z') || (text[i+6] >= 'A' && text[i+6] <= 'Z'))) {
+        text[i+3] = 'i';
+        text[i+4] = 's';
+        j = i + 5;
+        while (text[j] != '\0') {
+            text[j] = text[j+1];
+            j++;
+        }
+        i += 5;
+    }
+
+    // Correct "I is" to "I am"
+    if ((text[i] == 'i' || text[i] == 'I') && (text[i+1] == ' ') && (text[i+2] == 'i' || text[i+2] == 'I') && (text[i+3] == 's' || text[i+3] == 'S') && !((text[i+4] >= 'a' && text[i+4] <= 'z') || (text[i+4] >= 'A' && text[i+4] <= 'Z'))) {
+        text[i+2] = 'a';
+        text[i+3] = 'm';
+        i += 4;
+    }
+
+    // Correct "they is" to "they are"
+    if ((text[i] == 't' || text[i] == 'T') && (text[i+1] == 'h' || text[i+1] == 'H') && (text[i+2] == 'e' || text[i+2] == 'E') && (text[i+3] == 'y' || text[i+3] == 'Y') && (text[i+4] == ' ') && (text[i+5] == 'i' || text[i+5] == 'I') && (text[i+6] == 's' || text[i+6] == 'S') && !((text[i+7] >= 'a' && text[i+7] <= 'z') || (text[i+7] >= 'A' && text[i+7] <= 'Z'))) {
+        text[i+5] = 'a';
+        text[i+6] = 'r';
+        j = i + 7;
+        if (text[j] == '\0') {
+            text[j+1] = '\0';
+        } else {
+            while (text[j] != '\0') {
+                j++;
+            }
+            text[j] = ' ';
+            text[j+1] = '\0';
+            while (j != i + 7) {
+                text[j] = text[j-1];
+                j--;
+            }
+        }
+        text[i+7] = 'e';
+        i += 8;
+    }
+
+    // Correct "we is" to "we are"
+    if ((text[i] == 'w' || text[i] == 'W') && (text[i+1] == 'e' || text[i+1] == 'E') && (text[i+2] == ' ') && (text[i+3] == 'i' || text[i+3] == 'I') && (text[i+4] == 's' || text[i+4] == 'S') && !((text[i+5] >= 'a' && text[i+5] <= 'z') || (text[i+5] >= 'A' && text[i+5] <= 'Z'))) {
+        text[i+3] = 'a';
+        text[i+4] = 'r';
+        if (text[i+5] == '\0') {
+            text[i+6] = '\0';
+        } else {
+            j = i + 5;
+            while (text[j] != '\0') {
+                j++;
+            }
+            text[j] = ' ';
+            text[j+1] = '\0';
+            while (j != i + 5) {
+                text[j] = text[j-1];
+                j--;
+            }
+        }
+        text[i+5] = 'e';
+        i += 6;
+    }
+
+    // Correct "she am" to "she is"
+    if ((text[i] == 's' || text[i] == 'S') && (text[i+1] == 'h' || text[i+1] == 'H') && (text[i+2] == 'e' || text[i+2] == 'E') && (text[i+3] == ' ') && (text[i+4] == 'a' || text[i+4] == 'A') && (text[i+5] == 'm' || text[i+5] == 'M') && !((text[i+6] >= 'a' && text[i+6] <= 'z') || (text[i+6] >= 'A' && text[i+6] <= 'Z'))) {
+        text[i+4] = 'i';
+        text[i+5] = 's';
+        i += 6;
+    }
+
+    // Correct "he were" to "he was"
+    if ((text[i] == 'h' || text[i] == 'H') && (text[i+1] == 'e' || text[i+1] == 'E') && (text[i+2] == ' ') && (text[i+3] == 'w' || text[i+3] == 'W') && (text[i+4] == 'e' || text[i+4] == 'E') && (text[i+5] == 'r' || text[i+5] == 'R') && (text[i+6] == 'e' || text[i+6] == 'E') && !((text[i+7] >= 'a' && text[i+7] <= 'z') || (text[i+7] >= 'A' && text[i+7] <= 'Z'))) {
+        text[i+4] = 'a';
+        text[i+5] = 's';
+        j = i + 6;
+        while (text[j] != '\0') {
+            text[j] = text[j+1];
+            j++;
+        }
+        i += 6;
+    }
+
+    // Correct "they was" to "they were"
+    if ((text[i] == 't' || text[i] == 'T') && (text[i+1] == 'h' || text[i+1] == 'H') && (text[i+2] == 'e' || text[i+2] == 'E') && (text[i+3] == 'y' || text[i+3] == 'Y') && (text[i+4] == ' ') && (text[i+5] == 'w' || text[i+5] == 'W') && (text[i+6] == 'a' || text[i+6] == 'A') && (text[i+7] == 's' || text[i+7] == 'S') && !((text[i+8] >= 'a' && text[i+8] <= 'z') || (text[i+8] >= 'A' && text[i+8] <= 'Z'))) {
+        text[i+5] = 'w';
+        text[i+6] = 'e';
+        text[i+7] = 'r';
+        j = i + 8;
+        if (text[j] == '\0') {
+            text[j+1] = '\0';
+        } else {
+            while (text[j] != '\0') {
+                j++;
+            }
+            text[j+1] = '\0';
+            text[j] = ' ';
+            while (j != i + 8) {
+                text[j] = text[j-1];
+                j--;
+            }
+        }
+        text[i+8] = 'e';
+        i += 9;
+    }
+
+    // Correct "I were" to "I was"
+    if ((text[i] == 'i' || text[i] == 'I') && (text[i+1] == ' ') && (text[i+2] == 'w' || text[i+2] == 'W') && (text[i+3] == 'e' || text[i+3] == 'E') && (text[i+4] == 'r' || text[i+4] == 'R') && (text[i+5] == 'e' || text[i+5] == 'E') && !((text[i+6] >= 'a' && text[i+6] <= 'z') || (text[i+6] >= 'A' && text[i+6] <= 'Z'))) {
+        text[i+2] = 'w';
+        text[i+3] = 'a';
+        text[i+4] = 's';
+        j = i + 5;
+        while (text[j] != '\0') {
+            text[j] = text[j+1];
+            j++;
+        }
+        i += 5;
+    }
+
+    // Correct "you was" to "you were"
+    if ((text[i] == 'y' || text[i] == 'Y') && (text[i+1] == 'o' || text[i+1] == 'O') && (text[i+2] == 'u' || text[i+2] == 'U') && (text[i+3] == ' ') && (text[i+4] == 'w' || text[i+4] == 'W') && (text[i+5] == 'a' || text[i+5] == 'A') && (text[i+6] == 's' || text[i+6] == 'S') && !((text[i+7] >= 'a' && text[i+7] <= 'z') || (text[i+7] >= 'A' && text[i+7] <= 'Z'))) {
+        text[i+4] = 'w';
+        text[i+5] = 'e';
+        text[i+6] = 'r';
+        j = i + 7;
+        if (text[j] == '\0') {
+            text[j+1] = '\0';
+        } else {
+            while (text[j] != '\0') {
+                j++;
+            }
+            text[j+1] = '\0';
+            text[j] = ' ';
+            while (j != i + 7) {
+                text[j] = text[j-1];
+                j--;
+            }
+        }
+        text[i+7] = 'e';
+        i += 8;
+    }
+
+    // Correct "we was" to "we were"
+    if ((text[i] == 'w' || text[i] == 'W') && (text[i+1] == 'e' || text[i+1] == 'E') && (text[i+2] == ' ') && (text[i+3] == 'w' || text[i+3] == 'W') && (text[i+4] == 'a' || text[i+4] == 'A') && (text[i+5] == 's' || text[i+5] == 'S') && !((text[i+6] >= 'a' && text[i+6] <= 'z') || (text[i+6] >= 'A' && text[i+6] <= 'Z'))) {
+        text[i+3] = 'w';
+        text[i+4] = 'e';
+        text[i+5] = 'r';
+        j = i + 6;
+        if (text[j] == '\0') {
+            text[j+1] = '\0';
+        } else {
+            while (text[j] != '\0') {
+                j++;
+            }
+            text[j+1] = '\0';
+            text[j] = ' ';
+            while (j != i + 6) {
+                text[j] = text[j-1];
+                j--;
+            }
+        }
+        text[i+6] = 'e';
+        i += 7;
+    }
+
+    // Correct "she were" to "she was"
+    if ((text[i] == 's' || text[i] == 'S') && (text[i+1] == 'h' || text[i+1] == 'H') && (text[i+2] == 'e' || text[i+2] == 'E') && (text[i+3] == ' ') && (text[i+4] == 'w' || text[i+4] == 'W') && (text[i+5] == 'e' || text[i+5] == 'E') && (text[i+6] == 'r' || text[i+6] == 'R') && (text[i+7] == 'e' || text[i+7] == 'E') && !((text[i+8] >= 'a' && text[i+8] <= 'z') || (text[i+8] >= 'A' && text[i+8] <= 'Z'))) {
+        text[i+5] = 'a';
+        text[i+6] = 's';
+        j = i + 7;
+        while (text[j] != '\0') {
+            text[j] = text[j+1];
+            j++;
+        }
+        i += 7;
+    }
+    }
+
+
 
     cout << "\n" << "Corrected text" << "\n";
     cout << "------------------------------------------------";
     cout << "\n\n" << text << "\n\n";
     cout << "------------------------------------------------" << "\n";
+    
 }
